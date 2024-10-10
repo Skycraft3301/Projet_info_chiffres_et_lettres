5b14ae164a1e6516539f2c229ea9db76e2cfd652
Skycraft3301 <Dlutinkijou@outlook.fr>
2024-10-10 11:32:30 +0200

    Version n°4

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@
 public class Main {
 public static void main (String[] args) {
 
-	System.out.println("hello");
+int score_joueurA = 0, score_joueurB = 0 ;
+
+System.out.println("hello");
+
+for (int i=1 ; i<=6 ; i++) {
+
+}
 
 }
 }
